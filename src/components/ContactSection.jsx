928eef41520185343cac2392d673e4bf290f685e
Julien Pons <jp93rom@orange.fr>
2023-10-10 12:04:18 +0200

    Add font-awesome packages and start building the contact section.

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -1,4 +1,11 @@
 import '../styles/ContactSection.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPhone } from '@fortawesome/free-solid-svg-icons'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
+import { faFolderOpen } from '@fortawesome/free-solid-svg-icons'
+import { faLocationDot } from '@fortawesome/free-solid-svg-icons'
 
 function ContactSection() {
   return (
@@ -8,7 +15,30 @@ function ContactSection() {
         <span className='contact-section-title'>CONTACT</span>
       </div>
       <div className='contact-section-content'>
-
+        <div className='contact-line-group'>
+          <div className='icon-wrapper'><FontAwesomeIcon icon={faPhone} /></div>
+          <span className='contact-line'>00.00.00.00.00</span>
+        </div>
+        <div className='contact-line-group'>
+          <div className='icon-wrapper'><FontAwesomeIcon icon={faEnvelope} /></div>
+          <span className='contact-line'>myemailaddress@testemail.com</span>
+        </div>
+        <div className='contact-line-group'>
+          <div className='icon-wrapper'><FontAwesomeIcon icon={faLinkedin} /></div>
+          <span className='contact-line'>@linkedinpseudo</span>
+        </div>
+        <div className='contact-line-group'>
+          <div className='icon-wrapper'><FontAwesomeIcon icon={faGithub} /></div>
+          <span className='contact-line'>Githubpseudo</span>
+        </div>
+        <div className='contact-line-group'>
+          <div className='icon-wrapper'><FontAwesomeIcon icon={faFolderOpen} /></div>
+          <span className='contact-line'>Portfolio</span>
+        </div>
+        <div className='contact-line-group'>
+          <div className='icon-wrapper'><FontAwesomeIcon icon={faLocationDot} /></div>
+          <span className='contact-line'>Ma région</span>
+        </div>
       </div>
     </div>
   )
