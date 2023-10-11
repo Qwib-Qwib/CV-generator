3c811eb97ec743fcb29693ca495384c3618043f1
Julien Pons <jp93rom@orange.fr>
2023-10-11 12:09:18 +0200

    Add props validation for contact section, and continue implementing its inputs.

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -1,5 +1,6 @@
 import '../styles/ContactSection.css'
 import { useState } from 'react'
+import PropTypes from 'prop-types'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPhone } from '@fortawesome/free-solid-svg-icons'
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
@@ -25,34 +26,51 @@ function ContactSection({focusedElement, handleEditClick, handleUpdateClick}) {
         <span className='contact-section-title'>CONTACT</span>
       </div>
       <div className='contact-section-content'>
-        <div className='neutral contact-line-group'>
+        <div className={focusedElement === 0 ? 'neutral contact-line-group' : 'contact-line-group'}>
           <div className='icon-wrapper'><FontAwesomeIcon icon={faPhone} /></div>
-          {focusedElement === 4 ? <Input /> : <span className='contact-line'>{phoneNumber}</span>}
+          {focusedElement === 4 ? <Input category='contact-tel' value={phoneNumber} handleChange={(e) => setPhoneNumber(e.target.value)} /> : <span className='contact-line'>{phoneNumber}</span>}
           <Button buttonType={'edit'} handleClick={() => handleEditClick(4)} />
+          {focusedElement === 4 ? <div className='button-wrapper'><Button buttonType={'update'} handleClick={handleUpdateClick} /></div> : null}
         </div>
-        <div className='neutral contact-line-group'>
+        <div className={focusedElement === 0 ? 'neutral contact-line-group' : 'contact-line-group'}>
           <div className='icon-wrapper'><FontAwesomeIcon icon={faEnvelope} /></div>
-          <span className='contact-line'>{email}</span>
+          {focusedElement === 5 ? <Input category='contact-email' value={email} handleChange={(e) => setEmail(e.target.value)} /> : <span className='contact-line'>{email}</span>}
+          <Button buttonType={'edit'} handleClick={() => handleEditClick(5)} />
+          {focusedElement === 5 ? <div className='button-wrapper'><Button buttonType={'update'} handleClick={handleUpdateClick} /></div> : null}
         </div>
-        <div className='neutral contact-line-group'>
+        <div className={focusedElement === 0 ? 'neutral contact-line-group' : 'contact-line-group'}>
           <div className='icon-wrapper'><FontAwesomeIcon icon={faLinkedin} /></div>
-          <span className='contact-line'>{linkedin}</span>
+          {focusedElement === 6 ? <Input category='contact-linkedin' value={linkedin} handleChange={(e) => setLinkedin(e.target.value)} /> : <span className='contact-line'>{linkedin}</span>}
+          <Button buttonType={'edit'} handleClick={() => handleEditClick(6)} />
+          {focusedElement === 6 ? <div className='button-wrapper'><Button buttonType={'update'} handleClick={handleUpdateClick} /></div> : null}
         </div>
-        <div className='neutral contact-line-group'>
+        <div className={focusedElement === 0 ? 'neutral contact-line-group' : 'contact-line-group'}>
           <div className='icon-wrapper'><FontAwesomeIcon icon={faGithub} /></div>
-          <span className='contact-line'>{github}</span>
+          {focusedElement === 7 ? <Input category='contact-github' value={github} handleChange={(e) => setGithub(e.target.value)} /> : <span className='contact-line'>{github}</span>}
+          <Button buttonType={'edit'} handleClick={() => handleEditClick(7)} />
+          {focusedElement === 7 ? <div className='button-wrapper'><Button buttonType={'update'} handleClick={handleUpdateClick} /></div> : null}
         </div>
-        <div className='neutral contact-line-group'>
+        <div className={focusedElement === 0 ? 'neutral contact-line-group' : 'contact-line-group'}>
           <div className='icon-wrapper'><FontAwesomeIcon icon={faFolderOpen} /></div>
-          <a href={portfolioLink} className='contact-line'>Portfolio</a>
+          {focusedElement === 8 ? <Input category='contact-portfolio' value={portfolioLink} handleChange={(e) => setPortfolioLink(e.target.value)} /> : <a href={portfolioLink} className='contact-line'>Portfolio</a>}
+          <Button buttonType={'edit'} handleClick={() => handleEditClick(8)} />
+          {focusedElement === 8 ? <div className='button-wrapper'><Button buttonType={'update'} handleClick={handleUpdateClick} /></div> : null}
         </div>
-        <div className='neutral contact-line-group'>
+        <div className={focusedElement === 0 ? 'neutral contact-line-group' : 'contact-line-group'}>
           <div className='icon-wrapper'><FontAwesomeIcon icon={faLocationDot} /></div>
-          <span className='contact-line'>{location}</span>
+          {focusedElement === 9 ? <Input category='contact-location' value={location} handleChange={(e) => setLocation(e.target.value)} /> : <span className='contact-line'>{location}</span>}
+          <Button buttonType={'edit'} handleClick={() => handleEditClick(9)} />
+          {focusedElement === 9 ? <div className='button-wrapper'><Button buttonType={'update'} handleClick={handleUpdateClick} /></div> : null}
         </div>
       </div>
     </div>
   )
 }
+
+ContactSection.propTypes = {
+  focusedElement: PropTypes.number,
+  handleEditClick: PropTypes.func,
+  handleUpdateClick: PropTypes.func
+};
 
 export default ContactSection;
