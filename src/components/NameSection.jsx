2b17c32040c0c4f405eeb6b8095f718d2d30dfbb
Julien Pons <jp93rom@orange.fr>
2023-10-12 16:58:09 +0200

    Improve conditional rendering of name section.

--- src/components/NameSection.jsx
+++ src/components/NameSection.jsx
@@ -5,30 +5,15 @@ import Button from './Button';
 import Input from './Input';
 
 function NameSection({focusedElement, handleEditClick, handleUpdateClick}) {
-  const [name, setName] = useState('John Doe')
+  const [name, setName] = useState('John Doe');
 
-  if (focusedElement === 0) {
-    return (
-      <div className="primary-neutral name-section">
-        <h1 className='name'>{name}</h1>
-        {focusedElement === 0 ? <div className='common-buttons-wrapper'><Button buttonType={'edit'} handleClick={handleEditClick} /></div> : null}
-      </div>
-
-    )
-  } else if (focusedElement === 1) {
-    return (
-      <div className='name-section'>
-        <Input category={'name'} value={name} handleChange={(e) => setName(e.target.value)} />
-        <div className='update-button-wrapper'>
-          <Button buttonType={'update'} handleClick={handleUpdateClick} />
-        </div>
-      </div>
-    )
-  } else {
-    return (
-      <h1 className="deactivated name-section">{name}</h1>
-    )
-  }
+  return (
+    <div className={focusedElement === 0 ? 'primary-neutral name-section' : 'name-section'}>
+      {focusedElement === 1 ? <Input category={'name'} value={name} handleChange={(e) => setName(e.target.value)} /> : <h1 className='name'>{name}</h1>}
+      {focusedElement === 0 ? <div className='common-buttons-wrapper'><Button buttonType={'edit'} handleClick={handleEditClick} /></div> : null}
+      {focusedElement === 1 ? <div className='update-button-wrapper'><Button buttonType={'update'} handleClick={handleUpdateClick} /></div> : null}
+    </div>
+  )
 }
 
 NameSection.propTypes = {
